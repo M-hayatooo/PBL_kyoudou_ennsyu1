b37ec9e97b9466597e6d544df4ed04bcf5c79930
M-hayatooo <82699320+M-hayatooo@users.noreply.github.com>
2021-12-20 12:34:30 +0900

    Update ennsyu1.py

--- ennsyu1.py
+++ ennsyu1.py
@@ -1,6 +1,6 @@
 from flask import *
+from werkzeug.utils import secure_filename
 import os
-
 # Flaskオブジェクトの生成
 app = Flask(__name__)
 
@@ -45,14 +45,79 @@ def div():
     b = int(request.form.get("b"))
     if (b==0):
         return "<h1>0では割れません</h1>" 
-    
-
     r = a / b
     return "<h1>" +str(a) +"÷"+str(b)+" ＝ " + str(r) + "</h1>" 
 
+def detect_text_uri(save_path):
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    with open(save_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    #print('Texts:')
+    '''
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    '''
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    return texts
+
+#-----ファイルのアップロード-----#
+#GETの処理
+@app.route('/up/', methods=['GET'])
+def up_get():
+    return render_template('up.html', message = '数式だけが載っている画像を送って下さい', flag = False)
+
+def ansserch(textinfo):
+    a = []
+    texts = textinfo
+    length = len(texts)
+    for count in range(length):
+        judge = texts[count].description.isdigit()
+        #print(hanntei)    
+        if judge == True:
+            a.append( int(texts[count].description) )
+    #print(a[0])    #print(a[1])
+    for count in range(length):
+        ennzann = texts[count].description
+        if ( ennzann == "+") or (ennzann == "＋"):        
+            r = int(a[0]) + int(a[1])
+        elif (ennzann == "-") or (ennzann == "－"):
+            r = int(a[0]) - int(a[1])
+        elif (ennzann == "×") or (ennzann == "✕") or (ennzann == "*") or (ennzann == "x"):
+            r = int(a[0]) * int(a[1]) 
+        elif (ennzann == "÷") or (ennzann == "/") or (ennzann == "／"):
+            r = int(a[0]) / int(a[1])
+    return r        
+
+#POSTの処理
+@app.route('/up/', methods=['POST'])
+def up_post():
+# ファイルのリクエストパラメータを取得
+    f = request.files.get('image')
+# ファイル名を取得
+    filename = secure_filename(f.filename)
+# ファイルを保存するディレクトリを指定
+    filepath = 'static/image/' + filename
+# ファイルを保存する
+    f.save(filepath)
+    #return render_template('up.html', title = 'Form Sample(post)', message = 'アップロードされた画像({})'.format(filename), flag = True, image_name = filename)
+    readinfo = detect_text_uri(filepath)
+    ans = ansserch(readinfo)
+    return "<h2>" + str(ans) + "</h2>"
 
 # サーバーを起動
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 9761))
-    app.run(debug=True, port=os.environ['PORT'], host="0.0.0.0")
-    
+    #port = int(os.environ.get("PORT", 5555))
+    #app.run(debug=True, port=os.environ['PORT'], host="0.0.0.0")
+    app.run(port=5555, debug=True)
